66ee065d2bcf99272d355abd9f15bde3ff255658
wesleyromijn <wesleyromijn@gmail.com>
2019-05-10 11:46:34 +0200

    werkt de css nu wel?

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Wesley Romijn - Muntz Assesment</title>
-    <link rel="stylesheet" type="text/css" src="style.css">
-  </head>
+    <link rel="stylesheet" type="text/css" src="/style.css" />
+  </head> 
   <body>
     <header>
     	<span class="myName"></span>
