665acdd29138d9aaae2e915526729ecaa991c277
wesleyromijn <wesleyromijn@gmail.com>
2019-05-10 11:32:57 +0200

    vanuit lokale server naar master

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Wesley Romijn - Muntz Assesment</title>
+    <link rel="stylesheet" type="text/css" src="style.css">
+  </head>
+  <body>
+    <header>
+    	<span class="myName"></span>
+    </header>
+    <div class="content-full"></div>
+    <footer>&copy; <?php echo date("d-m-Y"); ?></footer>
+
+    <script type="text/javascript">
+    	var myName = "Wesley Romijn";
+    	document.querySelector('.myName').innerHTML = myName;
+    </script>
+  </body>
+</html>
